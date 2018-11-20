86080513604b8aea14ae9f4fd4458863ed449f0a
AlfieriChou <alfierichou@gmail.com>
2018-11-20 16:45:29 +0800

    error handle

--- schemas/user.js
+++ schemas/user.js
@@ -30,19 +30,24 @@ const userResolvers = {
   Query: {
     users: async (_, { filter = {} }) => {
       const users = await User.find({}, null, filter)
-      return users.map(user => user)
+      return users
     },
     user: async (_, { id }) => {
       const user = await User.findById(id)
+      if (!user) throw new Error('用户不存在')
       return user
     }
   },
   Mutation: {
     addUser: async (_, { input }) => {
-      const user = await User.create(input)
-      return user
+      const exists = await User.findOne({ phone: input.phone })
+      if (exists) throw new Error('该手机号已存在')
+      const result = await User.create(input)
+      return result
     },
     editUser: async (_, { id, input }) => {
+      const exists = await User.findOne({ id: id })
+      if (!exists) throw new Error('用户不存在')
       const user = await User.findByIdAndUpdate(id, input)
       return user
     },
